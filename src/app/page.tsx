77dcf9c1781033284a0239f77ace97cce48c6426
iwobrzyski <iwo.brzyski98@gmail.com>
2025-10-13 16:10:01 +0200

    change head and translations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -107,7 +107,7 @@ export default function Home() {
             <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center bg-white/90 backdrop-blur-md p-8 rounded-xl shadow-2xl border border-green-200 z-50 w-[75%] max-w-lg">
               <div className="mb-6">
                 <div className="text-3xl font-bold bg-gradient-to-r from-green-500 to-teal-500 text-transparent bg-clip-text mb-3">
-                  Gratulacje! Wygrałaś!
+                  Gratulacje, wygrałaś!
                 </div>
                 <div className="text-lg font-normal text-gray-600">
                   Mam nadzieję, że zapisywałaś po drodze kolejność, bo
